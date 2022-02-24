b074aabf50d4d8a5a2ce674cdbe1a58cdff5b173
wallanmota <wallanmota.13@gmail.com>
2022-02-24 11:26:55 -0300

    ex04

---
+++ src/main/kotlin/ex04.kt
@@ -0,0 +1,16 @@
+fun main(){
+    /*
+    WHILE
+    Uma empresa desenvolveu uma pesquisa para saber as características psicológicas dos indivíduos de uma região.
+    Para tanto, a cada uma das pessoas era perguntado: idade, sexo (1-feminino / 2-masculino / 3-Outros),
+    e as opções (1, se a pessoa era calma; 2, se a pessoa era nervosa e 3, se a pessoa era agressiva).
+    Pede-se para elaborar um sistema que permita ler os dados de 150 pessoas, calcule e mostre:
+      * número de pessoas calmas;
+      * número de mulheres nervosas;
+      * número de homens agressivos;
+      * número de outros calmos;
+      * número de pessoas nervosas com mais de 40 anos;
+      * número de pessoas calmas com menos de 18 anos.
+
+     */
+}
