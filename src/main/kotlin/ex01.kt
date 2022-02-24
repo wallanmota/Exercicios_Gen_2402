b00a971b12bfd4f5a540d739003e2b3725d83ddb
wallanmota <wallanmota.13@gmail.com>
2022-02-24 11:05:10 -0300

    ex01

--- src/main/kotlin/ex01.kt
+++ src/main/kotlin/ex01.kt
@@ -1,3 +1,12 @@
 fun main() {
+    /*
+    FOR
+    Informar todos os números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5.
+     */
 
+    for(i in 1000..1999){
+        if(i % 11 == 5){
+            print("$i, ")
+        }
+    }
 }
